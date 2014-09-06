fa09e4b27ce93c0f3699e8328ccc57b18fff8584
chuc3 <chuc3@rpi.edu>
2014-09-06 14:32:35 -0400

    initial stuff

---
+++ Assets/Scripts/spawnWalls.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spawnWalls : MonoBehaviour {
+
+	public Transform wall;
+	public Transform pellet;
+	public TextAsset map;
+
+	void Awake(){
+		Vector3 v = new Vector3 ();
+		int i, j;i = 0;j = 0;
+		v.y = 1;
+
+		string [] lines = map.text.Split ('\n');
+		foreach (string line in lines) 
+		{
+			foreach(char c in line)
+			{
+				if(c=='X'){
+					v.x=i;v.z=j;
+					Instantiate(wall,v,Quaternion.identity);
+				}
+				else if(c=='.'){
+					v.x=i;v.z=j; v.y =2;
+					Instantiate(pellet,v,Quaternion.identity);
+					v.y = 1;
+				}
+				i+=1;
+			}
+			i=0;
+			j-=1;
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/eatPellet.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class eatPellet : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	void OnTriggerEnter (Collider other) {
+		if (other.tag == "nom") {
+			print ("hit");
+			Destroy (other.gameObject);
+		} 
+
+	}
+}
